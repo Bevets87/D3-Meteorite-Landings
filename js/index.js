4b513da71f1e0ec409353d1144fb09b2972aa65b
Steve <bevets87@gmail.com>
2017-07-06 19:52:44 -0400

    added promises

--- js/index.js
+++ js/index.js
@@ -2,7 +2,26 @@
 
   'use strict'
 
-  const drawSVG = function (dimensions) {
+  const getWorldMapData = function () {
+    return new Promise (function (resolve, reject) {
+      d3.json('https://d3js.org/world-50m.v1.json', function(error, mapData) {
+        if (error) reject(error)
+        resolve(mapData)
+      })
+    })
+  }
+
+  const getMeteoriteData = function (mapData) {
+    return new Promise (function (resolve, reject) {
+       d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json', function(error, meteoriteData) {
+         if (error) reject(error)
+         resolve({mapData: mapData, meteoriteData: meteoriteData})
+       })
+    })
+  }
+
+  const drawSVG = function (mapData, meteoriteData, dimensions) {
+    meteoriteData = meteoriteData.features
     const w = dimensions.width
     const h = dimensions.height
 
@@ -24,92 +43,74 @@
     .attr('class', 'tooltip')
     .style('opacity', 0)
 
+    svg.append('g')
+    .selectAll('path')
+    .data(topojson.feature(mapData, mapData.objects.countries).features)
+    .enter()
+    .append('path')
+    .attr('fill', 'black')
+    .attr('stroke', 'white')
+    .attr('d', path)
+    .style('opacity',0.5)
 
-     const renderWorldMap = function () {
-       return new Promise(function (resolve,reject) {
-         d3.json('https://d3js.org/world-50m.v1.json', function(json) {
-           svg.append('g')
-            .selectAll('path')
-            .data(topojson.feature(json, json.objects.countries).features)
-            .enter()
-            .append('path')
-            .attr('fill', 'black')
-            .attr('stroke', 'white')
-            .attr('d', path)
-            .style('opacity',0.5)
-            resolve(svg)
-          })
-        })
+    svg.append('g')
+    .selectAll('path')
+    .data(meteoriteData)
+    .enter()
+    .append('path')
+    .attr('d', path.pointRadius(d => Math.sqrt(d.properties.mass/10000)))
+    .attr('fill', 'green')
+    .style('cursor','pointer')
+    .on('mouseover', function (d) {
+      div.transition()
+      .duration(100)
+      .style('opacity', 0.9)
+      div.html('<h1>' + d.properties.name + '</h1><h3>' + new Date(d.properties.year) + '</h3>')
+      .style('left', d3.event.pageX + 'px')
+      .style('top',  d3.event.pageY + 'px')
+      .on('mouseout', function(d) {
+        div.transition()
+        .duration(400)
+        .style('opacity', 0)
+      })
+    })
+
+    const zoom = d3.zoom()
+    .scaleExtent([1, 5])
+    .on('zoom', () => d3.selectAll('g').attr('transform', d3.event.transform));
+    svg.call(zoom);
+
+  }
+
+  getWorldMapData().then(function (mapData) {
+    getMeteoriteData(mapData).then(function (data) {
+      const mapData = data.mapData
+      const meteoriteData = data.meteoriteData
+
+      if (window.innerWidth < 1000 && window.innerWidth >= 600) {
+        drawSVG(mapData, meteoriteData,{width: 600, height: 400})
+      } else if (window.innerWidth < 600) {
+        drawSVG(mapData, meteoriteData,{width: 350, height: 350})
+      } else {
+        drawSVG(mapData, meteoriteData,{width: 800, height: 500})
       }
-      const renderMeteorites = function () {
-        return new Promise( function (resolve,reject) {
-           d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json', function(json) {
-             json = json.features
-             svg.append('g')
-             .selectAll('path')
-             .data(json)
-             .enter()
-             .append('path')
-             .attr('d', path.pointRadius(d => Math.sqrt(d.properties.mass/10000)))
-             .attr('fill', 'green')
-             .style('cursor','pointer')
-             .on('mouseover', function (d) {
-               div.transition()
-               .duration(100)
-               .style('opacity', 0.9)
-               div.html('<h1>' + d.properties.name + '</h1><h3>' + new Date(d.properties.year) + '</h3>')
-               .style('left', d3.event.pageX + 'px')
-               .style('top',  d3.event.pageY + 'px')
-               .on('mouseout', function(d) {
-                 div.transition()
-                 .duration(400)
-                 .style('opacity', 0)
-               })
-             })
-             resolve(svg)
-           })
-         })
-       }
-       renderWorldMap().then(renderMeteorites)
-       const zoom = d3.zoom()
-       .scaleExtent([1, 5])
-       .on('zoom', () => d3.selectAll('g').attr('transform', d3.event.transform));
-       svg.call(zoom);
-     }
-
-
-window.addEventListener('orientationchange', function () {
-  if (screen.orientation.angle === 90) {
-    drawSVG({width: 600, height: 400})
-  } else {
-    drawSVG({width: 350, height: 350})
-  }
-})
-window.addEventListener('load', function () {
-  if (this.innerWidth < 1000 && this.innerWidth >= 600) {
-    drawSVG( {width: 600, height: 400})
-  } else if (this.innerWidth < 600) {
-    drawSVG({width: 350, height: 350})
-  } else {
-    drawSVG({width: 800, height: 500})
-  }
-})
-window.addEventListener('resize', function () {
-  if (this.innerWidth < 1000 && this.innerWidth >= 600) {
-    drawSVG({width: 600, height: 400})
-  } else if (this.innerWidth < 600) {
-    drawSVG({width: 350, height: 350})
-  } else {
-    drawSVG({width: 800, height: 500})
-  }
-})
-
-
-
-
-
-
-
-
+      window.addEventListener('orientationchange', function () {
+        if (screen.orientation.angle === 90) {
+          drawSVG(mapData, meteoriteData,{width: 600, height: 400})
+        } else {
+          drawSVG(mapData, meteoriteData,{width: 350, height: 350})
+        }
+      })
+      window.addEventListener('resize', function () {
+        if (this.innerWidth < 1000 && this.innerWidth >= 600) {
+          drawSVG(mapData, meteoriteData,{width: 600, height: 400})
+        } else if (this.innerWidth < 600) {
+          drawSVG(mapData, meteoriteData,{width: 350, height: 350})
+        } else {
+          drawSVG(mapData, meteoriteData,{width: 800, height: 500})
+        }
+      })
+    })
+  })
 
 }(d3, topojson))
